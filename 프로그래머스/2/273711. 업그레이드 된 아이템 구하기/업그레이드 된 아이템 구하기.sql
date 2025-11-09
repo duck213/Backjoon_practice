4bc0dd7d2b23690403fc7b9f99ca0c352ea7f559
Jung Deokkyu <76250852+duck213@users.noreply.github.com>
2025-11-09 22:41:36 +0900

    [level 2] Title: 업그레이드 된 아이템 구하기, Time: , Memory: undefined -BaekjoonHub

---
+++ 프로그래머스/2/273711. 업그레이드 된 아이템 구하기/업그레이드 된 아이템 구하기.sql
@@ -0,0 +1,9 @@
+select t.item_id, i.item_name, i.rarity
+from item_info i
+join item_tree t on t.item_id=i.item_id
+where t.parent_item_id in (
+    select item_id
+    from item_info
+    where rarity='RARE' 
+)
+order by t.item_id desc;
